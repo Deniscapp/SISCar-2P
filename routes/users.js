6d1381d0634d5858cc1931d2034cedaabafa38b3
deniscapp <denis_cappelini@hotmail.com>
2017-06-15 20:53:33 -0300

    user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const users = require('../controllers/users');
+const User = require('../models/user');
+const passport = require('passport');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  const query = User.find();
+  const promise = query.exec();
+  promise.then(doc => {
+    res.send(doc);
+  });
+
 });
+
+router.post('/signin', passport.authenticate('local'), users.signin );
+
+router.post('/signup', users.signup);
+
+router.get('/logout', users.logout);
+
 
 module.exports = router;
