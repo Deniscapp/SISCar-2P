fc67be16e911fdc11e20d3c2726de0ea10bdc655
deniscapp <denis_cappelini@hotmail.com>
2017-07-20 21:23:37 -0300

    new api key

--- public/javascripts/peer-init.js
+++ public/javascripts/peer-init.js
@@ -1,6 +1,6 @@
 const connections = [];
 
-var peer = new Peer({ key: 'oz9b3ni30qtcsor', debug: 2, config: {'iceServers': [
+var peer = new Peer({ key: 'gm6f69wwmi', debug: 2, config: {'iceServers': [
     { url: 'stun:stun.l.google.com:19302' },
     { url: 'turn:192.158.29.39:3478?transport=udp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username:'28224511:1379330808' },
     { url: 'turn:192.158.29.39:3478?transport=tcp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username:'28224511:1379330808' }
